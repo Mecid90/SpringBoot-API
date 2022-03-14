63bb7cd78503604609d4c838f4f09ea634b10f06
Mecid90 <mecidm90@gmail.com>
2022-03-14 15:59:37 +0400

    SpringBoot(API)

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -18,6 +19,15 @@ public class StudentService {
 
     public List<Student> getStudents(){
         return studentRepository.findAll();
+    }
+
+
+    public void addNewStudent(Student student) {
+       Optional<Student> studentOptional = studentRepository.findByEmail(student.getEmail());
+       if(studentOptional.isPresent()){
+           throw new IllegalStateException("email taken");
+       }
+       studentRepository.save(student);
     }
 }
 
