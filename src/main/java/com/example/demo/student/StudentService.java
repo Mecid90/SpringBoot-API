48c9ffbc20845837aa97186d41a0b7827848577f
Mecid90 <mecidm90@gmail.com>
2022-03-14 16:58:34 +0400

    SpringBoot(API)

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -29,5 +29,13 @@ public class StudentService {
        }
        studentRepository.save(student);
     }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists){
+            throw new IllegalStateException("student with id " + studentId + " doesn't exists");
+        }
+        studentRepository.deleteById(studentId);
+    }
 }
 
